ea12562d0118234e36550744588b59d610f3e28e
Djafta Muendane <djafta@outlook.com>
2022-01-05 12:35:13 +0200

    Loading next/Image dimensions

--- src/comp/loading.js
+++ src/comp/loading.js
@@ -6,7 +6,7 @@ const Loading = () => {
     return (
         <div className={styles.loading} id={'loading'}>
             <div className={styles.loading_content}>
-                <Image className={styles.loading_img} height={'10px'} width={'10px'} src='/lirix-dark.png' alt=''/>
+                <Image className={styles.loading_img} height={'100px'} width={'100px'} src='/img/lirix-dark.png' alt=''/>
             </div>
         </div>
     )
